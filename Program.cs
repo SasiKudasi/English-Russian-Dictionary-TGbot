18c86b17e7fb2273e9487fe1e0b9675de3185f71
SasaiKudasai <sasha.mittal@yandex.ru>
2023-09-11 22:21:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Exceptions;
+using System.Threading;
+
+namespace tgbot
+{
+
+    internal class Program
+    {
+        static Tutor tutor = new Tutor();
+        static WordStorage storage = new WordStorage();
+        static TelegramBotClient Bot = new TelegramBotClient("6462476904:AAElPcodQl2MSSco0ENNI_1IOZVkDZ953QI");
+        static Dictionary<long, string> lastWord = new Dictionary<long, string>();
+        const string COMMAND_LIST =
+            @"Список команд: 
+            /add <eng><rus> - добавление английского слова и
+             его перевод в словарь.
+            /get - получение случайного слова.";
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }
+            };
+
+            Bot.StartReceiving(HandleUpdateAsync, HandleErrorAsync, receiverOptions, cancellationToken: cts.Token);
+
+            Console.ReadLine();
+           
+        }
+
+        private static Task HandleErrorAsync(ITelegramBotClient Bot, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegra API ERROR: \n " +
+                $"{apiRequestException.ErrorCode}\n" +
+                $" {apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+            Console.WriteLine(errorMessage);
+            return Task.CompletedTask;
+        }
+
+
+        static string AddWord(String[] msgArr)
+        {
+            if (msgArr.Length != 3)
+            {
+                return "Неправильное колличество аргументов. их должно быть 2";
+            }
+            else
+            {
+                tutor.AdWord(msgArr[1], msgArr[2]);
+                return "новое слово добавленно в словарь";
+            }
+        }
+
+        private static string GetRandomWord(long chatID)
+        {
+            var text = tutor.GetRandomWord();
+            if (lastWord.ContainsKey(chatID))
+            {
+                lastWord[chatID] = text;
+            }
+            else 
+                lastWord.Add(chatID, text);
+            return text;
+        }
+
+
+        private static async Task HandleUpdateAsync(ITelegramBotClient Bot, Update update, CancellationToken cancellationToken)
+        {
+            if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message &&  update.Message!.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                var chatId = update.Message.Chat.Id;
+                var messageText = update.Message.Text;
+                var firstName = update.Message.From.FirstName;
+                var msgArgs = messageText.Split(' ');
+                Message sendMessage;
+                String text;
+                switch (msgArgs[0])
+                {
+                    case "/start":
+                        text = COMMAND_LIST;
+                        break;
+                    case "/add":
+                        text = AddWord(msgArgs);
+                        break;
+                    case "/get":
+                        text = GetRandomWord(chatId);
+                        break;
+                    default:
+                        if (lastWord.ContainsKey((chatId)))
+                        {
+
+                            text = CheckWord(lastWord[chatId], msgArgs[0]);
+                            var newWord = GetRandomWord((chatId));
+                            text = $"{text}\r\nСледующее слово {newWord}";
+                        }
+                        else
+                        {
+                            text = COMMAND_LIST;
+                        }
+                        break;
+                }
+               sendMessage = await Bot.SendTextMessageAsync(chatId, text, cancellationToken: cancellationToken);
+               
+            }
+        }
+
+        private static string CheckWord(string eng, string rus)
+        {
+            if (tutor.CheckWord(eng, rus))               
+                return "Правильно!";                
+            else
+            {
+                var correctAnswer = tutor.Translate(eng);
+                return $"Неверно! Правильный ответ {correctAnswer}";
+            }
+        }
+    }
+}
---
+++ Tutor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tgbot
+{
+    internal class Tutor
+    {
+
+        private WordStorage _storage = new WordStorage();
+        private Dictionary<string, string> _dic;
+        private Random _random = new Random();
+
+
+        public Tutor()
+        {
+            _dic = _storage.GetAllWords();
+        }
+
+        public void AdWord(string eng, string rus)
+        {
+            if (!_dic.ContainsKey(eng))
+            {
+
+                _storage.AddWord(eng, rus);
+                _dic.Add(eng, rus);
+            }
+        }
+
+        public bool CheckWord(string eng, string rus)
+        {
+            var answer = _dic[eng];
+            return answer == rus;
+        }
+
+        public string Translate(string eng)
+        {
+            if (_dic.ContainsKey(eng))
+            {
+
+
+                return _dic[eng];
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public string GetRandomWord()
+        {
+            var r = _random.Next(0, _dic.Count);
+
+            var keys = new List<string>(_dic.Keys);
+
+            Console.WriteLine(keys[r]);
+            return keys[r];
+        }
+    }
+}
